7a81373e35b0900d47fbd422532540c2883bd051
thephotogenicbug <navinkumarachari@gmail.com>
2021-06-26 15:30:45 +0530

    EDUPROV: 26-06-2021 : Redesign Content user interface 2/2

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 $(document).ready(function(){function o(){verticalOffset="undefined"!=typeof verticalOffset?verticalOffset:0,element=$("body"),offset=element.offset(),offsetTop=offset.top,$("html, body").animate({scrollTop:offsetTop},30,"linear")}$(function(){$(document).on("scroll",function(){$(window).scrollTop()>100?$(".scroll-top-wrapper").addClass("show"):$(".scroll-top-wrapper").removeClass("show")}),$(".scroll-top-wrapper").on("click",o)})});
+
+/*Navbar JS  */
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+});
