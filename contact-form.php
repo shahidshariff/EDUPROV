b0a6136dfd6903a0b4802c8a180e4d76c548aef7
thephotogenicbug <navinkumarachari@gmail.com>
2021-09-01 17:37:12 +0530

    Eduprov : 01-09-2021 : Add Margin-top

--- contact-form.php
+++ contact-form.php
@@ -124,7 +124,7 @@
     <?php }?>
 
             <form action="" name="contactForm" method="post" enctype="multipart/form-data">
-              <div class="container">
+              <div class="container mt-5">
                 <div class="row">
                   <div class="col-md-12"></div>
 
